ef99a13745e8fde3ea5c9a31895cb61d8ee128a9
kingtitan <igw22003@byui.edu>
2024-01-13 21:13:36 -0800

    finish project

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter number: ");
+        string valueFromUser = Console.ReadLine();
+
+        int x =int.Parse(valueFromUser);
+        int y = 2;
+
+        if (x>y)
+        
+        {
+            Console.WriteLine("Greater");
+        }
+        else if (x<y)
+        {
+            Console.WriteLine("less");
+
+        }
+        else
+        {
+            Console.WriteLine("equal");
+        
+        }
     }
 }
