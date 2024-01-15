e6c6cbecacd02bbaca6d55bb0f5dbbf9b90e2cc4
kingtitan <igw22003@byui.edu>
2024-01-15 00:53:14 -0800

    finished project

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Net;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("what is your magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator= new Random();
+        int magicNumber =randomGenerator.Next(1, 101);
+
+        int guess =-1;
+
+        //Console.Write("what is your guess? ");
+        //int  guess = int.Parse(Console.ReadLine());
+        
+        
+        
+    
+        while (magicNumber != guess)
+
+         
+        
+            
+        
+
+        
+        {
+            Console.Write("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+    
+        
+            if (magicNumber > guess)
+            {
+               Console.WriteLine("higher");
+            }
+
+            else if (magicNumber < guess)
+
+            {
+                Console.WriteLine("lesser");
+            }
+        
+            else 
+            {
+                Console.WriteLine("you guessed right!");
+                
+            }
+        
+            
+               
+        }
+          
+        
     }
+    
+        
 }
