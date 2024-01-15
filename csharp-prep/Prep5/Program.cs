f41056560cc6bd3365003e96b2925f257549184e
kingtitan <igw22003@byui.edu>
2024-01-15 05:11:58 -0800

    finished project

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Xml.XPath;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        WelcomeMessage();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber= squareNumber(userNumber);
+        DisplayResult (userName, squaredNumber);
+
+    }
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("welcome to the programe !");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("please enter your name:");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("please enter your favorite number:");
+        int number =int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int squareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}" );
     }
 }
